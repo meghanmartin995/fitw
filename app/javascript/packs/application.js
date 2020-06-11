2e45cf46b214437f9d15137e8c088ac0786ebfd8
Meghan Martin <meghanxmartin@gmail.com>
2020-06-11 21:23:06 +0200

    fixes responsiveness

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,5 @@ import "bootstrap"
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // import { letters } from '../packs/letters'
+
+import { masonry } from '../packs/masonry'
---
+++ app/javascript/packs/masonry.js
@@ -0,0 +1,16 @@
+var elem = document.querySelector('.grid');
+
+var masonry = new Masonry( elem, {
+  itemSelector: '.grid-item',
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  // percentPosition: true,
+  isFitWidth: true
+});
+
+imagesLoaded( elem ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  masonry.layout();
+});
+
+export default masonry
