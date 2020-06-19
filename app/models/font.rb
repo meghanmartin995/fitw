4ad586cf51094ec86e8aabf91c3c1972068002d3
Meghan Martin <meghanxmartin@gmail.com>
2020-06-19 19:44:07 +0200

    adds info to modal

--- app/models/font.rb
+++ app/models/font.rb
@@ -7,7 +7,8 @@ class Font < ApplicationRecord
   has_many :extwebsites, through: :extwebsite_fonts
   has_many :font_types
   has_many :types, through: :font_types
-  #validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, presence: true
   include PgSearch::Model
   multisearchable against: [:name, :designer]
   pg_search_scope :search_by_name_and_designer_and_websites,
