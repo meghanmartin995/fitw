c9ca1b0f1dc37046ec6a2b05b7db50a535c340f3
Meghan Martin <meghanxmartin@gmail.com>
2020-06-21 22:01:47 +0200

    fixes scroll

--- app/javascript/packs/masonry.js
+++ app/javascript/packs/masonry.js
@@ -1,3 +1,44 @@
+//-------------------------------------//
+
+// var grid = document.querySelector('.grid');
+
+// var msnry = new Masonry( grid, {
+//   itemSelector: 'none',
+//   columnWidth: '..grid-sizer',
+//   isFitWidth: true,
+//   stagger: 30,
+//   visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+//   hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+// });
+
+
+// // initial items reveal
+// imagesLoaded( grid, function() {
+//   grid.classList.remove('are-images-unloaded');
+//   msnry.options.itemSelector = '.grid-item';
+//   var items = grid.querySelectorAll('.grid-item');
+//   msnry.appended( items );
+// });
+
+
+// // init Infinte Scroll
+
+// var infScroll = new InfiniteScroll( grid, {
+//   path: "a[rel='next']",
+//   append: '.grid-item',
+//   outlayer: msnry,
+//   status: '.page-load-status',
+// });
+
+
+
+
+
+
+
+
+
+
 var elem = document.querySelector('.grid');
 
 if (elem) {
@@ -6,43 +47,35 @@ var masonry = new Masonry( elem, {
   itemSelector: '.grid-item',
   columnWidth: '.grid-sizer',
   horizontalOrder: true,
-  isFitWidth: true
+  isFitWidth: true,
+  stagger: 30,
+  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
 });
 
-function masonryUpdate() {
-  setTimeout(function() {
-    const masonry = Masonry.data('.grid')
-    masonry.reloadItems();
-    imagesLoaded( elem ).on( 'progress', function() {
-    masonry.layout();
+  imagesLoaded( elem ).on( 'progress', function() {
+  masonry.layout();
   });
-  },500);
-}
-
-function imageLoad() {
-    imagesLoaded( elem ).on( 'progress', function() {
-    masonry.layout();
-    });
-  }
- imageLoad()
 
 document.addEventListener('turbolinks:load', () => {
   var elem = document.querySelector('.grid');
 
-  if (elem) {
   var masonry = new Masonry( elem, {
     itemSelector: '.grid-item',
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     horizontalOrder: true,
-    isFitWidth: true
+    isFitWidth: true,
+    stagger: 30,
+    visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+    hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
   });
-  }
+
     imagesLoaded( elem ).on( 'progress', function() {
     masonry.layout();
     });
 });
-}
+
 
 var infScroll = new InfiniteScroll( elem, {
 
@@ -51,5 +84,6 @@ var infScroll = new InfiniteScroll( elem, {
   history: false,
   outlayer: masonry,
 });
+}
 
 export default masonry
