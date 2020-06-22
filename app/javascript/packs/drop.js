d34d04a4046b0210805369d9ba97db7a7a140263
Meghan Martin <meghanxmartin@gmail.com>
2020-06-22 21:49:53 +0200

    arrow animation

--- app/javascript/packs/drop.js
+++ app/javascript/packs/drop.js
@@ -4,29 +4,41 @@ const arrow = document.querySelector("#carrot")
 const label = document.querySelectorAll(".form-check-label")
 const drop = document.querySelectorAll(".dropped")
 const check = document.querySelectorAll(".form-check-input")
+const number = document.querySelector("#dropdownMenuLink span")
+
+
+dropdownlg.addEventListener("click", function() {
+  arrow.classList.add("animate__heartBeat")
+  arrow.classList.add("drop")
+  dropdown.classList.add("bold")
+})
+
+
 
 
 if (drop) {
 drop.forEach(function(d) {
-    d.addEventListener("change", function() {
+    d.addEventListener("change", function(event) {
     d.classList.toggle("bold")
+    const isChecked = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    if (isChecked > 0) {
+      console.log(isChecked)
+      number.classList.add("num-show")
+      dropdown.classList.add("bold")
+    }
+    else if (isChecked === 0) {
+      number.classList.remove("num-show")
+      dropdown.classList.remove("bold")
+      arrow.classList.remove("animate__heartBeat")
+      arrow.classList.remove("drop")
+    }
   })
 })
 }
 
 
-if (dropdown) {
-  dropdown.addEventListener("click", function() {
-    dropdown.classList.toggle("bold")
-    arrow.classList.toggle("animate__heartBeat")
-    arrow.classList.toggle("drop")
-  })
-}
-
-
-
 $('#myDropdown').on('hide.bs.dropdown', function () {
-    dropdown.classList.remove("bold")
+    //dropdown.classList.remove("bold")
     arrow.classList.remove("animate__heartBeat")
     arrow.classList.remove("drop")
 });
